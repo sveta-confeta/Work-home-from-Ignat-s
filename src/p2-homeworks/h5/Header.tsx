f979969fdcb476d31d4547cbf0608f3c6ba208dd
sveta-confeta <_svet_@tut.by>
2022-02-02 23:22:45 +0300

    HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import s from './HW5.module.css';
 import picture from './add.png'
@@ -6,20 +6,16 @@ import picture from './add.png'
 
 
 function Header() {
-  let[show,setShow]=useState<boolean>(false);
- const click=()=>setShow(true!==show);
-
     return (
-        <>
+
         <ul className={s.headerList}>
            <li className={s.item}><NavLink to={"/junior"} className={classHeaderList=>classHeaderList.isActive ? s.active : ' '}> Junior </NavLink></li>
             <li className={s.item}><NavLink to={"/junior-plus"} className={classHeaderList=>classHeaderList.isActive ? s.active : ' '}> JuniorPlus </NavLink></li>
             <li className={s.item}><NavLink to={"/pre-junior"} className={classHeaderList=>classHeaderList.isActive ? s.active : ' '}> PreJunior </NavLink></li>
 
-            <img onClick={click} className={s.plus} src={picture}/>
+            <img className={s.plus} src={picture}/>
         </ul>
 
-        </>
     )
 }
 
